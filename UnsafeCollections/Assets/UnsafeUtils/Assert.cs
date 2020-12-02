62ec36b8f9ab09783f478969e1c73d0c16b531f2
Dennis <denniscorvers@hotmail.com>
2020-12-02 20:18:54 +0100

    Add DebuggerStepThrough so debugger stops at the target that calls Assert

--- UnsafeCollections/Assets/UnsafeUtils/Assert.cs
+++ UnsafeCollections/Assets/UnsafeUtils/Assert.cs
@@ -25,67 +25,95 @@ THE SOFTWARE.
 using System;
 using System.Diagnostics;
 
-public class AssertException : Exception {
-  public AssertException() {
-  }
+public class AssertException : Exception
+{
+    public AssertException()
+    {
+    }
 
-  public AssertException(string msg)
-    : base(msg) {
-  }
+    public AssertException(string msg)
+      : base(msg)
+    {
+    }
 }
 
-public static class Assert {
-  [Conditional("DEBUG")]
-  public static void Fail() {
-    throw new AssertException();
-  }
-
-  [Conditional("DEBUG")]
-  public static void Fail(string error) {
-    throw new AssertException(error);
-  }
-
-  [Conditional("DEBUG")]
-  public static void Fail(string format, params object[] args) {
-    throw new AssertException(string.Format(format, args));
-  }
-
-  [Conditional("DEBUG")]
-  public static void Check(bool condition) {
-    if (condition == false) {
-      throw new AssertException();
+public static class Assert
+{
+    [Conditional("DEBUG")]
+    [DebuggerStepThrough]
+    public static void Fail()
+    {
+        throw new AssertException();
     }
-  }
 
-  [Conditional("DEBUG")]
-  public static void Check(bool condition, string error) {
-    if (condition == false) {
-      throw new AssertException(error);
+    [Conditional("DEBUG")]
+    [DebuggerStepThrough]
+    public static void Fail(string error)
+    {
+        throw new AssertException(error);
     }
-  }
 
-  [Conditional("DEBUG")]
-  public static void Check(bool condition, string format, params object[] args) {
-    if (condition == false) {
-      throw new AssertException(string.Format(format, args));
+    [Conditional("DEBUG")]
+    [DebuggerStepThrough]
+    public static void Fail(string format, params object[] args)
+    {
+        throw new AssertException(string.Format(format, args));
     }
-  }
 
-  public static void Always(bool condition) {
-    if (condition == false) {
-      throw new AssertException();
+    [Conditional("DEBUG")]
+    [DebuggerStepThrough]
+    public static void Check(bool condition)
+    {
+        if (condition == false)
+        {
+            throw new AssertException();
+        }
     }
-  }
 
-  public static void Always(bool condition, string error) {
-    if (condition == false) {
-      throw new AssertException(error);
+    [Conditional("DEBUG")]
+    [DebuggerStepThrough]
+    public static void Check(bool condition, string error)
+    {
+        if (condition == false)
+        {
+            throw new AssertException(error);
+        }
     }
-  }
 
-  public static void Always(bool condition, string format, params object[] args) {
-    if (condition == false) {
-      throw new AssertException(string.Format(format, args));
+    [Conditional("DEBUG")]
+    [DebuggerStepThrough]
+    public static void Check(bool condition, string format, params object[] args)
+    {
+        if (condition == false)
+        {
+            throw new AssertException(string.Format(format, args));
+        }
     }
-  }
+
+    [DebuggerStepThrough]
+    public static void Always(bool condition)
+    {
+        if (condition == false)
+        {
+            throw new AssertException();
+        }
+    }
+
+    [DebuggerStepThrough]
+    public static void Always(bool condition, string error)
+    {
+        if (condition == false)
+        {
+            throw new AssertException(error);
+        }
+    }
+
+    [DebuggerStepThrough]
+    public static void Always(bool condition, string format, params object[] args)
+    {
+        if (condition == false)
+        {
+            throw new AssertException(string.Format(format, args));
+        }
+    }
 }
